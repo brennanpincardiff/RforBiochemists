b57ce3ee80ace3feb50e9a61bb3eddcb3401950c
Paul Brennan <brennanpincardiff@gmail.com>
2015-05-05 15:20:54 +0100

    New Script for high res figs
    Journals require high resolution. This uses the tiff() function to do
    this.

--- export600dpiGraph.R
+++ export600dpiGraph.R
@@ -1,6 +1,24 @@
 # Publication quality graphs require 600dpi
-ppi=600    #pixels per square inch
-png("output.png", width=6*ppi, height=5*ppi, res=ppi)  
-#this creates the output file and adds information about size and resolution 
+dpi=600    #pixels per square inch
+tiff("output.tif", width=6*dpi, height=5*dpi, res=dpi)  # key function
+#the tiff() function creates the output file and adds information about size and resolution 
 
-dev.off()
+# data
+prot <- c(0.000, 0.016, 0.031, 0.063, 0.125, 0.250, 0.500, 1.000, 0.000, 0.016, 0.031, 0.063, 0.125, 0.250, 0.500, 1.000) 
+abs <- c(0.329, 0.352, 0.349, 0.379, 0.417, 0.491, 0.668, 0.956, 0.327, 0.341, 0.355, 0.383, 0.417, 0.446, 0.655, 0.905)
+
+#draw the graph:
+plot(abs~prot, 
+     xlab = "[Protein] (microg/ml)",
+     ylab = "Absorbance (570nm)",
+     main = "Protein Assay")
+
+line <- lm(abs~prot) # Calculate the line using the linear model function
+abline(line) # Draw the line
+
+r2 <- round(summary(line)$r.squared, 3)
+mylabel = bquote(italic(R)^2 == .(format(r2, digits = 3)))
+text(x = 0.2, y = 0.9, labels = mylabel)
+
+dev.off()  #very important command that tells R that you are finished plotting
+# otherwise your graph will not show up.
